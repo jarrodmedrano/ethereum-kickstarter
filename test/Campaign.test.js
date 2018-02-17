438f036a896b5dc82721fc6823ae6985b9c11a82
jmedran <jmedran@gmail.com>
2018-02-17 10:10:31 -0700

    adding test script

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -13,8 +13,29 @@ let campaign;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
-
+  //deploy a new version of the contract, not specifying an address
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
     .send({from: accounts[0], gas: '1000000'});
+
+  await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000'
+  });
+  //deploy a new contract and instruct web3 about its existence
+  //use destructuring to take first element out of array and assign to campaignAddress variable
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+  campaign = await new web3.eth.Contract(
+    //pass in abi interface for compiled campaign
+    JSON.parse(compiledCampaign.interface),
+    //pass in address of where the campaign exists
+    campaignAddress
+  );
+});
+
+describe('Campaigns', () => {
+  it('deploys a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
 });
