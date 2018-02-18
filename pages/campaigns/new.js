30af22a7f9a75b71f7dded56d96227452ff8aa3e
jmedran <jmedran@gmail.com>
2018-02-17 19:49:46 -0700

    adding validation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,23 +1,28 @@
 import React, {Component} from 'react';
-import { Input, Form, Button } from 'semantic-ui-react';
+import { Input, Form, Button, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
-    minimumContribution: 0
+    minimumContribution: 0,
+    errorMessage: ''
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods
-      .createCampaign(this.state.minimumContribution)
-      .send({
-        from: accounts[0]
-      })
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        })
+    } catch (err) {
+        this.setState({errorMessage: err.message})
+    }
   }
 
   render() {
@@ -26,11 +31,12 @@ class CampaignNew extends Component {
       <h3>
         Create a Campaign
       </h3>
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input label="wei" labelPosition="right" value={this.state.minimumContribution} onChange={event => this.setState({minimumContribution: event.target.value})} />
           </Form.Field>
+          <Message error header="Oops!" content={this.state.errorMessage} />
           <Button primary>Create!</Button>
         </Form>
       </Layout>
