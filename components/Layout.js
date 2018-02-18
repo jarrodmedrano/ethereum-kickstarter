e875f5e0c6ee7979bcd87feda170820325893be7
jmedran <jmedran@gmail.com>
2018-02-17 18:44:22 -0700

    adding head tag

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
 import Header from './Header';
 
 export default props  => {
   return (
     <Container style={{paddingTop: "10px"}}>
+      <Head><link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" /></Head>
       <Header />
       {props.children}
-      <h1>I'm a footer</h1>
     </Container>
   )
 }
