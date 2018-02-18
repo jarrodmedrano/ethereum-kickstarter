7d2808b2a2dbfe4dc461e8c4602e9b240a4fc8b1
jmedran <jmedran@gmail.com>
2018-02-17 19:00:08 -0700

    adding event on input

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,10 @@ import { Input, Form, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: 0
+  };
+
   render() {
     return (
       <Layout>
@@ -12,7 +16,7 @@ class CampaignNew extends Component {
         <Form>
           <Form.Field>
             <label>Minimum Contribution</label>
-            <Input label="wei" labelPosition="right" />
+            <Input label="wei" labelPosition="right" value={this.state.minimumContribution} onChange={event => this.setState({minimumContribution: event.target.value})} />
           </Form.Field>
           <Button primary>Create!</Button>
         </Form>
