f99f5afa545d22b52b8b8c81652e25732f1271e5
jmedran <jmedran@gmail.com>
2018-02-17 18:53:38 -0700

    adding label

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import { Input, Form, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
   render() {
     return (
       <Layout>
-      <h1>
-        New Campaign
-      </h1>
+      <h3>
+        Create a Campaign
+      </h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input label="wei" labelPosition="right" />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
       </Layout>
     );
   }
