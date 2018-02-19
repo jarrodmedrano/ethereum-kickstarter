8b25938ad960550fa703bc81be833043b4288d24
jmedran <jmedran@gmail.com>
2018-02-19 09:40:23 -0700

    modifying routes for requests

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class RequestNew extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+
+export default RequestNew;
