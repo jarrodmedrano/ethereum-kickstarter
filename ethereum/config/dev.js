9e589a6284a43fd614e4a38461cb16497c24b760
jmedran <jmedran@gmail.com>
2018-02-19 18:47:53 -0700

    encrypt keys

---
+++ ethereum/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mnemonic: 'ivory slot cupboard youth just maple clerk ostrich what spoil awake census',
+  network: 'https://rinkeby.infura.io/971OlJMZje92x4AbRd0H',
+  address: '0x8B7f238509D72C67D341c53d26621c147693f845'
+};
